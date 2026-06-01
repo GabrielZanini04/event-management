960b4b20cd14c105357269767f7ad66d5e9df2a8
GabrielZanini04 <gabrielzanini03@gmail.com>
2026-06-01 15:56:17 -0300

    implementa CRUD para eventos e participantes, adiciona recursos e validações

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Event;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,38 +14,95 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        $query = Event::query();
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach($relations as $relation){
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
+        return EventResource::collection(
+            $query->latest()->paginate());
     }
+
+
+    protected function shouldIncludeRelation(string $relation): bool
+    {
+            $include = request()->query('include');
+
+            if(!$include) {
+                return false;
+            }
+
+            $relations = array_map('trim', explode(',',$include));
+
+            return in_array($relation, $relations);
+    }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request ->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:time'
+        ]);
+
+        $event = Event::create([
+            ...$request ->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:time'
+             ]),
+             'user_id' => 1
+        ]);
+
+        return new EventResource($event);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+         $event->update(
+            $request ->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:time'
+             ])
+        );
+
+        return new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event ->delete();
+
+        return response()->json([
+            'message' => 'Event deleted succesfully'
+        ]);
     }
 }
